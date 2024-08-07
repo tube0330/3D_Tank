8cfebeeb9c25a8c0770277bcdd1c9e7bdd5636ee
tube0330 <zien0516@naver.com>
2024-08-07 11:16:46 +0900

    240807

--- Assets/02.Scripts/TurretCtrl.cs
+++ Assets/02.Scripts/TurretCtrl.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class TurretCtrl : MonoBehaviour
 {
     Transform tr;
+    float rotSpeed = 5f;
+
     void Start()
     {
         tr = transform;
@@ -16,5 +18,17 @@ public class TurretCtrl : MonoBehaviour
         /*화면상의 2차원 좌표를 3차원 세계 좌표의 광선으로 변환하는 과정*/
 
         Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 2000f, 1 << 8))
+        {
+            Vector3 relative = tr.InverseTransformPoint(hit.point); //맞았던 월드 위치를 탱크에 맞는 로컬좌표로 바꿈
+
+            float angle = Mathf.Atan2(relative.x, relative.z) * Mathf.Rad2Deg; //atan2() 함수를 이용해 두 점 간의 각도를 구함. 라디언각도를 일반각도로 바꿈
+
+            tr.Rotate(0f, angle * Time.deltaTime * rotSpeed, 0f); //회전
+        }
+
     }
 }
