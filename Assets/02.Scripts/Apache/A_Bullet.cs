49b1d249af5bd8afc07a71420a2e2f3cc9b436db
tube0330 <zien0516@naver.com>
2024-09-10 17:34:17 +0900

    240910_Timeline

--- Assets/02.Scripts/Apache/A_Bullet.cs
+++ Assets/02.Scripts/Apache/A_Bullet.cs
@@ -17,22 +17,15 @@ public class A_Bullet : MonoBehaviour
 
     void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.CompareTag("Player"))
-        {
-            Destroy(gameObject);
-            StartCoroutine(ExplosionCannon(3.0f));
-        }
-
-        else
-            StartCoroutine(ExplosionCannon(3.0f));
+        StartCoroutine(Explosion(0.5f));
 
     }
 
-    IEnumerator ExplosionCannon(float time)
+    IEnumerator Explosion(float time)
     {
         yield return new WaitForSeconds(time);
-        col.enabled = false;
         GameObject eff = Instantiate(Resources.Load<GameObject>("BigExplosionEffect"), transform.position, Quaternion.identity);
+        //col.enabled = false;
         Destroy(eff, 1.0f);
         Destroy(this.gameObject, 1.0f);
     }
--- Assets/02.Scripts/Apache/ApacheCtrl.cs
+++ Assets/02.Scripts/Apache/ApacheCtrl.cs
@@ -8,6 +8,10 @@ public class ApacheCtrl : MonoBehaviour
     public float movespeed = 0f;
     public float rotSpeed = 0f;
     public float verticalSpeed = 0f;
+    public GameObject bulletPrefab; // 총알 프리팹을 할당할 변수
+    public Transform firePoint; // 총알이 발사될 위치
+    public float fireRate = 0.5f; // 발사 간격
+    private float nextFire = 0f; // 다음 발사 시간
 
     void Start()
     {
@@ -16,29 +20,19 @@ public class ApacheCtrl : MonoBehaviour
 
     void Update()
     {
-        #region Apache A, D 좌우회전
-        if (Input.GetKey(KeyCode.A))    //눌렀을 때
-            rotSpeed += -0.05f;               //rotSpeed value minus --1
+        HandleMovement();
+        HandleRotation();
+        HandleVerticalMovement();
+        HandleFiring();
+    }
 
-        else if (Input.GetKey(KeyCode.D))
-            rotSpeed += 0.05f;               //rotSpeed value plus --2
-
-        else    //누르지 않았을 때
-        {
-            if (rotSpeed > 0f) rotSpeed += -0.05f;
-            else if (rotSpeed < 0f) rotSpeed += 0.05f;      //rotSpeed value minus -> plus --1
-        }
-
-        tr.Rotate(Vector3.up * rotSpeed * Time.deltaTime);  //rotSpeed value plus -> minus --2
-        #endregion
-
+    void HandleMovement()
+    {
         #region Apache W, S 앞뒤 이동
         if (Input.GetKey(KeyCode.W))
             movespeed += 0.02f;
-
         else if (Input.GetKey(KeyCode.S))
             movespeed += -0.02f;
-
         else
         {
             if (movespeed > 0f) movespeed += -0.02f;
@@ -47,14 +41,32 @@ public class ApacheCtrl : MonoBehaviour
 
         tr.Translate(Vector3.forward * movespeed * Time.deltaTime, Space.Self);
         #endregion
+    }
 
+    void HandleRotation()
+    {
+        #region Apache A, D 좌우회전
+        if (Input.GetKey(KeyCode.A))    // 눌렀을 때
+            rotSpeed += -0.05f;               // rotSpeed value minus --1
+        else if (Input.GetKey(KeyCode.D))
+            rotSpeed += 0.05f;               // rotSpeed value plus --2
+        else    // 누르지 않았을 때
+        {
+            if (rotSpeed > 0f) rotSpeed += -0.05f;
+            else if (rotSpeed < 0f) rotSpeed += 0.05f;      // rotSpeed value minus -> plus --1
+        }
+
+        tr.Rotate(Vector3.up * rotSpeed * Time.deltaTime);  // rotSpeed value plus -> minus --2
+        #endregion
+    }
+
+    void HandleVerticalMovement()
+    {
         #region Apache Z, C 위아래 이동
-        if (Input.GetKey(KeyCode.Z)) //up
+        if (Input.GetKey(KeyCode.Z)) // up
             verticalSpeed += 0.02f;
-
         else if (Input.GetKey(KeyCode.C))
             verticalSpeed += -0.02f;
-
         else
         {
             if (verticalSpeed > 0f) verticalSpeed += -0.02f;
@@ -62,5 +74,22 @@ public class ApacheCtrl : MonoBehaviour
         }
         tr.Translate(Vector3.up * verticalSpeed * Time.deltaTime, Space.World);
         #endregion
+    }
+
+    void HandleFiring()
+    {
+        if (Input.GetMouseButtonDown(0) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            FireBullet();
+        }
+    }
+
+    void FireBullet()
+    {
+        if (bulletPrefab && firePoint)
+        {
+            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        }
     }
 }
